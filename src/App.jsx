6b345e0620633ba2cc7ecbb4e6942e569c39055e
monikharga <monikharga@gmail.com>
2026-07-02 09:04:47 +0000

    add hello

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,13 @@ import { useEffect, useState } from 'react';
 // import { db } from './firebase'; // Import the db we exported earlier
 import { collection, getDocs } from 'firebase/firestore'; 
 import './App.css';
-
+import Hello from "./Hello"
 function App() {
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    // Example: Fetching a collection called 'users' from Firestore
-    const fetchData = async () => {
-      const querySnapshot = await getDocs(collection(db, "users"));
-      querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => `, doc.data());
-      });
-      setLoading(false);
-    };
-
-    fetchData();
-  }, []);
 
   return (
     <>
-      <h1>Firebase is connected!</h1>
-      {loading ? <p>Loading data...</p> : <p>Data logged to console.</p>}
+      <Hello/>
     </>
   );
 }
---
+++ src/Hello.jsx
@@ -0,0 +1,8 @@
+const Hello =()=>{
+    return (
+        <>
+        <h1>hi</h1>
+        </>
+    )
+}
+export default Hello;
